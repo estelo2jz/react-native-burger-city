c00c78be364a80b6ced8c1da91dcd8cc3d636151
esteloabell <manitobaaa@gmail.com>
2021-06-01 19:59:47 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+import Onboarding from './src/screens/Onboarding';
+
+import * as Font from 'expo-font';
+
+const customFonts = {
+  'MontserratBold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  'MontserratRegular': require('./assets/fonts/Montserrat-Regular.ttf'),
+}
 
 export default function App() {
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+  const _loadAssetsAsync = async () => {
+    await Font.loadAsync(customFonts);
+    setAssetsLoaded(true);
+  }
+  
+  useEffect(() => {
+    _loadAssetsAsync();
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Burger City UI!</Text>
-      <StatusBar style="auto" />
+      { assetsLoaded ? <Onboarding /> : <ActivityIndicator size="small" />}
     </View>
   );
 }
