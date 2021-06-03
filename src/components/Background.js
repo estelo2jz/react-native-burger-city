ccef6c0e9b720057a713d567119ca439382ad760
esteloabell <manitobaaa@gmail.com>
2021-06-03 18:03:45 -0500

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 
-const Background = () => {
+const {width, height} = Dimensions.get('window');
+
+const Background = ({params}) => {
   return (
-    <View>
-      <Text>background</Text>
+    <View style={styles.container}>
+      <View style={styles.top_shadow}></View>
+
+      <View style={styles.bottom_shadow}></View>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f7',
+  },
+  top_shadow: {
+    position: 'absolute',
+    top: 0,
+    backgroundColor: '#f5f5f7',
+    width: width,
+    height: 10,
+    shadowColor: 'red',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 5,
+  }
+});
+
 export default Background
 
-const styles = StyleSheet.create({})
