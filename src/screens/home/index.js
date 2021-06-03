e3d6de6e4658554463e186d176c183f9d3f797ac
esteloabell <manitobaaa@gmail.com>
2021-06-02 19:52:54 -0500

    Create index.js

---
+++ src/screens/home/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const HomeScreen = ({params}) => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          width: 250,
+          height: 100,
+          backgroundColor: '#000000',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+        <Text style={{ color: '#ff9f1c', fontSize: 22 }}>Home Screen</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default HomeScreen;
