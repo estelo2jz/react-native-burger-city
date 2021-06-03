f481533c65f3cdde3c853e026d97e9cd4f98f8cd
esteloabell <manitobaaa@gmail.com>
2021-06-02 21:22:59 -0500

    Update BurgersStack.js

--- src/navigation/BurgersStack.js
+++ src/navigation/BurgersStack.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BurgersScreen from '../screens/burgers';
+import HeaderTitle from '../components/HeaderTitle';
 
 const BurgersStack = ({ params }) => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Burgers Screen" component={BurgersScreen} />
+      <Stack.Screen name="Burgers" component={BurgersScreen} 
+        options={{ headerTitle: props => <HeaderTitle /> }}
+      />
     </Stack.Navigator>
   );
 };
