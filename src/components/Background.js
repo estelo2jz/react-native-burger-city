495c0e24dada26122cd4887e19f9767cd89a8cf8
esteloabell <manitobaaa@gmail.com>
2021-06-03 17:50:25 -0500

    Create Background.js

---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Background = () => {
+  return (
+    <View>
+      <Text>background</Text>
+    </View>
+  )
+}
+
+export default Background
+
+const styles = StyleSheet.create({})
