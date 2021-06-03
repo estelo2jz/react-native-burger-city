8323bc9a8089b83b9794e444279f51cf7bdaec39
esteloabell <manitobaaa@gmail.com>
2021-06-02 21:23:10 -0500

    Update HomeStack.js

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/home';
+import HeaderTitle from '../components/HeaderTitle';
 
 const HomeStack = ({ params }) => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home Screen" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} 
+        options={{headerTitle: props => <HeaderTitle />}}
+      />
     </Stack.Navigator>
   );
 };
