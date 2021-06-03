0a3af79eb9438ce78de963521182b89ec4a6370a
esteloabell <manitobaaa@gmail.com>
2021-06-02 21:22:21 -0500

    Create HeaderRight.js

---
+++ src/components/HeaderRight.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HeaderRight = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default HeaderRight
+
