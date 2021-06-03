35e1a5dc359447fe14cf7066f19f0cafa62d2d04
esteloabell <manitobaaa@gmail.com>
2021-06-03 12:35:16 -0500

    Update HeaderLanguageChange.js

--- src/components/HeaderLanguageChange.js
+++ src/components/HeaderLanguageChange.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 
-const HeaderLanguageChange = () => {
+const HeaderLanguageChange = ({onPress,params}) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity style={{ marginLeft: 10, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}
+      onPress={onPress}
+    >
+      <Text style={{ fontFamily: 'MontserratBold', color: '#ff9f1c', fontSize: 18}}>EN</Text>
+      <Image style={{ marginLeft: 3 }} source={require('../../assets/images/drop-down16.png')} />
+    </TouchableOpacity>
   )
 }
 
@@ -13,5 +16,5 @@ const styles = StyleSheet.create({
   
 });
 
-export default HeaderLanguageChange
+export default HeaderLanguageChange;
 
