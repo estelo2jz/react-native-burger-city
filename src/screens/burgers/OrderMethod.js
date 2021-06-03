3fe7d9db89fb7ff8d987802b7d3652ad71f043d2
esteloabell <manitobaaa@gmail.com>
2021-06-03 17:50:56 -0500

    Create OrderMethod.js

---
+++ src/screens/burgers/OrderMethod.js
@@ -0,0 +1,42 @@
+import React, {useLayoutEffect} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import Background from '../../components/Background';
+import HeaderRight from '../../components/HeaderRight'; 
+import HeaderLanguageChange from '../../components/HeaderLanguageChange';
+
+
+const OrderMethod = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <HeaderRight onPress={() => { navigation.navigate('wallet') }} />,
+      headerLeft: () => (<HeaderLanguageChange onPress={() => (Alert.alert('Do something'))} />)
+    })
+  })
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          width: 250,
+          height: 100,
+          backgroundColor: '#000000',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+        <Text style={{ color: '#ff9f1c', fontSize: 22 }}>Burgers Screen</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default OrderMethod;
