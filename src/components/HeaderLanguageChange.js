d10af7dd7a5c8547ee85de5e4d03f08113475875
esteloabell <manitobaaa@gmail.com>
2021-06-03 11:31:26 -0500

    Create HeaderLanguageChange.js

---
+++ src/components/HeaderLanguageChange.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HeaderLanguageChange = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default HeaderLanguageChange
+
