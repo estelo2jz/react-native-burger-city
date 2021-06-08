acc2640b28ed67562b918fdcea95a536a1d9fe56
esteloabell <manitobaaa@gmail.com>
2021-06-08 08:01:32 -0500

    Update BurgersStack.js

--- src/navigation/BurgersStack.js
+++ src/navigation/BurgersStack.js
@@ -3,6 +3,8 @@ import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OrderMethodScreen from '../screens/burgers/OrderMethod';
 import DeliveryAddressScreen from '../screens/burgers/DeliveryAddress';
+import DeliveryAddressConfirmedScreen from '../screens/burgers/DeliveryAddressConfirmed';
+import MenuScreen from '../screens/burgers/Menu';
 import HeaderTitle from '../components/HeaderTitle';
 
 const BurgersStack = ({ params }) => {
@@ -14,6 +16,12 @@ const BurgersStack = ({ params }) => {
         options={{ headerTitle: props => <HeaderTitle /> }}
       />
       <Stack.Screen name="DeliveryAddressScreen" component={DeliveryAddressScreen}
+        options={{ headerTitle: props => <HeaderTitle /> }}
+      />
+      <Stack.Screen name="DeliveryAddressConfirmedScreen" component={DeliveryAddressConfirmedScreen}
+        options={{ headerTitle: props => <HeaderTitle /> }}
+      />
+      <Stack.Screen name="MenuScreen" component={MenuScreen}
         options={{ headerTitle: props => <HeaderTitle /> }}
       />
     </Stack.Navigator>
