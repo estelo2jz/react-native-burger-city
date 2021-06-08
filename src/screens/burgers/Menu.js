236a9cafd1c5cf7a9ec66dbfba082e437b22debd
esteloabell <manitobaaa@gmail.com>
2021-06-08 12:16:05 -0500

    Update Menu.js

--- src/screens/burgers/Menu.js
+++ src/screens/burgers/Menu.js
@@ -1,10 +1,11 @@
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 import Background from '../../components/Background';
 import HeaderRight from '../../components/HeaderRight';
 import HeaderBack from '../../components/HeaderBack';
 import Title from '../../components/Title';
+import Button from '../../components/Button';
 
 const Menu = ({ params, navigation, route }) => {
 
@@ -19,31 +20,109 @@ const Menu = ({ params, navigation, route }) => {
         <HeaderBack
           onPress={() => { navigation.goBack() }}
         />
-      )
-    })
-  })
+      ),
+    });
+  });
 
-  return (
-    <Background>
-      <View style={styles.container}>
-        <View style={styles.addressView}>
-          <Title 
-            title="Delivery Address" 
-            subTitle="No. 02, 6th Lane, Colomo 03"
-            titleColor="#ffffff"
-            subTitleColor="#ff9f1c"
-          ></Title>
-        </View>
-        <View style={styles.dateTimeView}>
-          <Title 
-            title="Delivery Date & Time" 
-            subTitle="No. 02, 6th Lane, Colomo 03"
-            titleColor="#ffffff"
-            subTitleColor="#ff9f1c"e
-          ></Title>
+  const categories = [
+    {
+      id: 1,
+      name: 'All values meals',
+      image:  require("../../../assets/images/meal1.png"),
+    },
+    {
+      id: 2,
+      name: 'Salads/sides',
+      image: require("../../../assets/images/salad.png"),
+    },
+    {
+      id: 3,
+      name: 'Desserts',
+      image: require("../../../assets/images/sweets.png"),
+    },
+    {
+      id: 4,
+      name: 'Beverages ',
+      image: require("../../../assets/images/drinks.png"),
+    }
+  ]
+
+  const _renderCategory = (item, index) => {
+    return (
+      <View 
+      key={index}
+        style={{ 
+          margin: 12, 
+          width: 160, 
+          height: 160, 
+          backgroundColor: '#ffffff',
+          borderRadius: 6,
+          shadowColor: '#000000',
+          shadowOffset: {width: 2, height: 2},
+          shadowOpacity: 0.1,
+          shadowRadius: 6,
+          elevation: 5
+
+        }}
+      >
+        <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+          <View style={{height: 100, justifyContent: 'center'}}>
+            <Image source={item.image}></Image>
+          </View>
+          <View style={{ width: 80, paddingTop: 8, alignItems: 'center' }}>
+            <Text
+              style={{
+                color: '#000000',
+                fontFamily: 'MontserratBold',
+                fontSize: 12,
+                lineHeight: 16,
+              }}
+            >{item.name}</Text>
+          </View>
         </View>
       </View>
-    </Background>
+    )
+  }
+
+  return (
+    <ScrollView>
+      <Background>
+        <View style={styles.container}>
+          <View style={styles.addressView}>
+            <Title 
+              title="Delivery Address" 
+              subTitle="No. 02, 6th Lane, Colomo 03"
+              titleColor="red"
+              style={{titleColor: '#ffffff'}}
+              subTitleColor="#FF9F1C"
+            ></Title>
+          </View>
+          <View style={styles.dateTimeView}>
+            <Title 
+              title="Delivery Date & Time" 
+              subTitle="No. 02, 6th Lane, Colomo 03"
+              titleColor="#ffffff"
+              subTitleColor="#FF9F1C"
+            ></Title>
+          </View>
+          <View style={styles.category}>
+            {
+              categories.map((category, index) => {
+                return _renderCategory(category, index);
+              })
+            }
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center', marginBottom: 20}}>
+            <Button 
+              text='Secret Menu' 
+              onPress={() => {
+                navigation.push('BurgersScreen')
+              }} 
+            />
+          </View>
+        </View>
+      </Background>
+    </ScrollView>
   )
 }
 
@@ -53,11 +132,22 @@ const styles = StyleSheet.create({
   },
   addressView: {
     height: 115,
-    backgroundColor: '#1d2126',
+    backgroundColor: '#ffffff',
+
   },
   dateTimeView: {
     height: 85,
-    backgroundColor: '#1c272e',
+    backgroundColor: '#ffffff',
+  },
+  category: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   }
 });
 
