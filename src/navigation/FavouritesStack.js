81a6d795df0854ca5d5598369724ee1f88fc6129
esteloabell <manitobaaa@gmail.com>
2021-06-25 21:17:51 -0500

    Update FavouritesStack.js

--- src/navigation/FavouritesStack.js
+++ src/navigation/FavouritesStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import FavouritesScreen from '../screens/favourites';
+import SelectItemScreen from '../screens/burgers/SelectItem';
 import HeaderTitle from '../components/HeaderTitle';
 
 
@@ -11,6 +12,9 @@ const FavouritesStack = ({ params }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Favourites" component={FavouritesScreen} 
+        options={{ headerTitle: props => <HeaderTitle /> }}
+      />
+      <Stack.Screen name="SelectItemScreen" component={SelectItemScreen}
         options={{ headerTitle: props => <HeaderTitle /> }}
       />
     </Stack.Navigator>
